5f54aa9b60f7c13213f58a7b1e4ff0fc566d3158
Alexander Riccio <test35965@gmail.com>
2019-05-29 22:46:36 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -21,7 +21,7 @@ def run_guessing_game
     if guess == random_number
       puts "You guessed the correct number!"
     else
-      puts "The computer guessed #{guess}."
+      puts "The computer guessed #{random_number}."
       #binding.pry
     end
   end

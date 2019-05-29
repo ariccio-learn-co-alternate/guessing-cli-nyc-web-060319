2aaa15dafd59cd5b82dddca7a67cb9fc3ba0d4d8
Alexander Riccio <test35965@gmail.com>
2019-05-29 22:28:01 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,6 +9,7 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     response = gets.chomp
     if response == "exit"
+      puts "Goodbye!"
       return
     end
     guess = response.to_i
